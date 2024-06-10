691edce169358534e3df954ab1f65e59aa2c807d
focta <focke.wulf7234@gmail.com>
2024-06-10 17:31:20 +0900

    prismaの設定を一旦完了した

--- README_FOR_ME.md
+++ README_FOR_ME.md
@@ -16,3 +16,57 @@ npx create-next-app@latest . --typescript --eslint
 ```
 npm i --save-dev @types/bcrypt prisma
 ```
+
+
+## prismaの初期化
+
+```
+npx prisma init --datasource-provider postgresql
+```
+
+なおvscodeでコードフォーマッタが効かないので、下記の記事を参考にフォーマットできるように設定した。
+https://www.kimizuy.dev/blog/post/format-schema-prisma
+
+上記コマンドの実行を終えると、プロジェクトに以下のファイルが作成されているので、それらを編集していく。
+
+- `prisma/schema.prisma`
+- `.env`
+
+### テーブルの設定(`prisma/schema.prisma`)
+
+追加・変更するテーブルの設定は`prisma/schema.prisma`に記載していく。
+
+### DB接続情報の追加(`.env`)
+
+### マイグレーションの実施
+
+```
+npx prisma migrate dev --name init
+```
+
+これにて、`prisma/migrations`のマイグレーションが作成される。
+
+### prisamのクライアントの作成
+
+prismaからDB操作を行うためのオブジェクトを生成する必要がある。
+
+これは以下のコマンドで発行できる。
+
+```
+npx prisma generate
+```
+
+### (補足)Prisma studioによるデータベースの確認
+
+下記コマンドを発行することで `prisma studio` が起動して、データベースの内容をブラウザ上で確認できる。
+
+```
+npx prisma studio
+```　
+
+# Next Authの設定
+
+## `.env`への設定追加
+
+以下の2つの設定を。
+
---
+++ prisma/migrations/20240610081815_init/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+    "hashPassword" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAcountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAcountId_key" ON "Account"("provider", "providerAcountId");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
